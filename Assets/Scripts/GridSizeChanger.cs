6e74237c7c7a4d779b0fddd1f97320c7c2089c04
MatthiasWK <matthias.wk@hotmail.com>
2018-08-30 13:50:55 +0200

    Initial commit

---
+++ Assets/Scripts/GridSizeChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSizeChanger : MonoBehaviour {
+
+    public Text Size;
+
+    private void Start()
+    {
+        Size.text = Constants.Rows + " x " + Constants.Columns;
+    }
+
+    public void ChangeGridSize(int Change)
+    {
+        
+        if (Change == 0)
+        {
+            Constants.Rows -= 1;
+            Constants.Columns -= 1;
+        }
+        else if (Change == 1)
+        {
+            Constants.Rows += 1;
+            Constants.Columns += 1;
+        }
+
+        Size.text = Constants.Rows + " x " + Constants.Columns;
+    }
+}
